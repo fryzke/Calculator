a50c9c437af02d93c5eba4e3cb6c56c3145743d8
fryzke <fryekdy@gmail.com>
2026-01-12 10:46:24 +0900

    fix: next.Double()후 버퍼 비우기 및 scanner.close() 추가

--- Calculator/App.java
+++ Calculator/App.java
@@ -58,6 +58,7 @@ public class App {
                 System.out.print("기준값을 입력하세요: ");
                 try {
                     double param = scanner.nextDouble();
+                    scanner.nextLine(); //버퍼 비우기
                     System.out.println("result: " + calc.searchLog(param));
                 }catch (InputMismatchException e){
                     System.out.println("error: 숫자만 입력하세요.");
@@ -68,7 +69,7 @@ public class App {
 
         }
         System.out.println("========종 료========");
-
+        scanner.close();
 
     }
 }
